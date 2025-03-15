76bf1190eb3c1a23cdd98e94647479591cceb305
Cheru Berhanu <cheru@cheru.dev>
2025-03-15 12:20:41 -0700

    add hacky mappings

--- main.go
+++ main.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"github.com/peterhellberg/acr122u"
+	"os"
 )
 
 func main() {
+	fileName := "mappings.json"
+	mappings := make(map[string]uint32)
+
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	if file != nil {
+		json.Unmarshal(file, &mappings)
+	}
+
 	ctx, err := acr122u.EstablishContext()
 	if err != nil {
 		panic(err)
 	}
 
+	cleanup := func() {
+		json, err := json.Marshal(mappings)
+		if err != nil {
+			panic(err)
+		}
+		os.WriteFile(fileName, json, 0644)
+	}
+
 	ctx.ServeFunc(func(c acr122u.Card) {
-		fmt.Printf("%x\n", c.UID())
+		var key string
+
+		fmt.Println("enter key: ")
+		_, err = fmt.Scanln(&key)
+		if err != nil {
+			if err.Error() == "unexpected newline" {
+				fmt.Println("saving!")
+				cleanup()
+				return
+			} else {
+				panic(err)
+			}
+		}
+
+		uid := binary.LittleEndian.Uint32(c.UID())
+		mappings[key] = uid
+		fmt.Printf("%s: %x\n", key, uid)
 	})
 }
